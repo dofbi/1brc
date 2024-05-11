fdc703dda32c9a27179ba85da761116a1b462bbc
dofbi.eth <genova@dofbi.com>
2024-05-11 21:41:50 +0000

    ✨ feat (func): add MinTemperature, MaxTemperature, AverageTemperature

--- src/main/go/dofbi/calc.go
+++ src/main/go/dofbi/calc.go
@@ -13,3 +13,32 @@ func ReadFile(filepath string) ([]byte, error) {
 
 	return body, nil
 }
+
+func MinTemperature(temps[]float64) float64{
+	min := temps[0]
+	for _, temp := range temps {
+		if (temp < min){
+			min = temp
+		}
+	}
+	return min
+}
+
+func MaxTemperature(temps[]float64) float64{
+	max := temps[0]
+	for _, temp := range temps {
+		if (temp > max){
+			max = temp
+		}
+	}
+	return max
+}
+
+func AverageTemperature(temps[]float64) float64{
+	total := 0.0
+	for _, temp := range temps {
+		total += temp
+	}
+	return total/float64(len(temps))
+
+}
--- src/main/go/dofbi/calc_test.go
+++ src/main/go/dofbi/calc_test.go
@@ -20,3 +20,32 @@ func TestFileReading(t *testing.T) {
 
 	fmt.Println("Ligne: ",len(data))
 }
+
+func TestMinTemperature(t *testing.T){
+	temps := []float64{12.0, 8.9, 38.8}
+	min := MinTemperature(temps)
+
+	if min != 8.9 {
+		t.Fatalf("Expected 8.9, got %f", min)
+	}
+}
+
+func TestMaxTemperature(t *testing.T){
+	temps := []float64{12.0, 8.9, 38.8}
+	min := MaxTemperature(temps)
+
+	if min != 38.8 {
+		t.Fatalf("Expected 38.8, got %f", min)
+	}
+	
+}
+
+func TestAverageTemperature(t *testing.T){
+	temps := []float64{12.0, 8.9, 38.8}
+	avg := AverageTemperature(temps)
+	expected :=(12.0 + 8.9 + 38.8)/3
+
+	if avg != expected {
+		t.Fatalf("Expected %f, got %f", expected, avg)
+	}
+}
